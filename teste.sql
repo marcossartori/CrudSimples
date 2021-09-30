56fa1eded1fe85d60c96087dfc3967c4a12ad4d5
marcossartori <infomarcossartori@gmail.com>
2021-09-30 09:50:45 -0300

    primeiro comit (subindo script sql)

--- teste.sql
+++ teste.sql
@@ -12,9 +12,40 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Exportação de dados foi desmarcado.
 
--- Exportação de dados foi desmarcado.
+-- Copiando estrutura do banco de dados para teste
+CREATE DATABASE IF NOT EXISTS `teste` /*!40100 DEFAULT CHARACTER SET utf8mb3 */;
+USE `teste`;
+
+-- Copiando estrutura para tabela teste.preco
+CREATE TABLE IF NOT EXISTS `preco` (
+  `idpreco` int(11) NOT NULL AUTO_INCREMENT,
+  `preco` float DEFAULT NULL,
+  PRIMARY KEY (`idpreco`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3;
+
+-- Copiando dados para a tabela teste.preco: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `preco` DISABLE KEYS */;
+INSERT INTO `preco` (`idpreco`, `preco`) VALUES
+	(7, 140),
+	(8, 140),
+	(9, 4.4),
+	(10, 2.2);
+/*!40000 ALTER TABLE `preco` ENABLE KEYS */;
+
+-- Copiando estrutura para tabela teste.produto
+CREATE TABLE IF NOT EXISTS `produto` (
+  `idprod` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) DEFAULT NULL,
+  `cor` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`idprod`)
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb3;
+
+-- Copiando dados para a tabela teste.produto: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `produto` DISABLE KEYS */;
+INSERT INTO `produto` (`idprod`, `nome`, `cor`) VALUES
+	(38, 'cachaça', 'amarelo');
+/*!40000 ALTER TABLE `produto` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
